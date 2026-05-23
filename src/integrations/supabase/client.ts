de5612c9510951a4ac4e02df722d731cdb346fed
ramonrealmac <135175171+ramonrealmac@users.noreply.github.com>
2026-05-23 01:17:43 -0300

    docker para instalar n oserver

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://zicapdmkddpllxniupgi.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InppY2FwZG1rZGRwbGx4bml1cGdpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUyNjE3MTYsImV4cCI6MjA5MDgzNzcxNn0.hqOi69cdFzB_G_JpWS-UvdusCKpeATSMOxci0dEas78";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL || "https://zicapdmkddpllxniupgi.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InppY2FwZG1rZGRwbGx4bml1cGdpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUyNjE3MTYsImV4cCI6MjA5MDgzNzcxNn0.hqOi69cdFzB_G_JpWS-UvdusCKpeATSMOxci0dEas78";
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
